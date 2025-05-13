7e50af3ed7d35f4493a3f288a0e8499bab8d21be
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2025-05-13 19:02:05 -0300

    first commit

---
+++ src/main/java/dev/tiagosilva/service/DeliveryService.java
@@ -0,0 +1,15 @@
+package dev.tiagosilva.service;
+
+public class DeliveryService {
+    public void createDelivery() {
+        System.out.println("Creating an delivery...");
+    }
+
+    public void updateDelivery() {
+        System.out.println("Updating an delivery...");
+    }
+
+    public void deleteDelivery() {
+        System.out.println("Deleting an delivery...");
+    }
+}
